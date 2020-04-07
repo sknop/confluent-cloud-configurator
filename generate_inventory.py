5fc3f10ca8c9260d280e5d56c3a887cf3020468d
Christoph Schubert <christoph.schubert@tu-dortmund.de>
2020-04-07 05:03:11 +0200

    removed superfluous conversion function

--- generate_inventory.py
+++ generate_inventory.py
@@ -14,14 +14,6 @@ import sys
 from jinja2 import Template
 
 
-# TODO: test how we deal with empty data fields (e.g. no control center nodes configured in terraform)
-def dictlist_to_listdic(dict_list):
-    """
-    converts a dictionary of lists to a list of dictionaries
-    """
-    return [dict(zip(dict_list, t)) for t in zip(*dict_list.values())]
-
-
 def get_values(raw_data, field_name):
     return raw_data[field_name]['value']
 
@@ -40,12 +32,10 @@ def parse_cp_data(raw_data):
 if __name__ == '__main__':
     raw_data = json.load(sys.stdin)
 
-    # TODO: move hard coded settings to terraform output variables
     ansible_vars = {
         'user': raw_data['ssh_username']['value'],
         'keyfile': raw_data['ssh_key']['value']
     }
-
 
     with open("hosts.j2") as template_file:
         template = Template(template_file.read())
